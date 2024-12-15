79f4b1093f4f4bbd2d7d416965e1625bbdf73b88
Raja <kraj9380286@gmail.com>
2024-12-15 15:19:43 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,1 +1,13 @@
-console.log("Raja")
+//use of forEach in arrays
+let obj={
+    "name":"raja",
+    "age":18,
+    "weight":85,
+    "height":"5ft",
+    Greet:function addnum(){
+console.log("Happy")
+}
+};
+console.log(obj);
+for(let key in obj){
+console.log(key)}
