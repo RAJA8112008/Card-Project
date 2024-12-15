74ba1cd2132e1fabd2d23c6e71f9879cac0957e2
Raja <kraj9380286@gmail.com>
2024-12-15 15:54:05 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,4 +1,11 @@
-let arr=[10,20,30,40]
-for(let value of arr){
-    console.log(value);
-}
+ let arr=[10,20,30,40]
+ function getsum(arr){
+    let sum=0;
+    arr.forEach((value)=>{
+        sum=sum+value;
+});
+return sum;
+ }
+let totalsum=getsum(arr);
+console.log(totalsum);
+ 
