1a1b5222211e84c66962771f9874c50f82281a9b
Raja <kraj9380286@gmail.com>
2024-12-16 23:26:49 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,8 +1,14 @@
 //map in practice
-let arr = [10, 3, 5, 6, 9, 75, 20, 30, 50];
-for(let value of arr){
-    console.log(value)
-};
+let arr =[10, 3, 5, 6, 9, 75, 20, 30, 50];
+function addvalue(arr){
+    let sum=0;
+arr.forEach((value)=>{
+sum=sum+value;
+})
+return sum;
+}
+let totalsum=addvalue(arr);
+console.log(totalsum);
 
 
 
