b1f50daf82a78d8a320cee31f2816c0423520c7e
Raja <kraj9380286@gmail.com>
2024-12-16 22:36:06 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,6 +1,8 @@
 //map in practice
 let arr=[10,20,30,50]
-arr.map((value)=>{
-    return value*value;
-   console.log(value+1)
-});
+
+let ans=arr.map((value)=>{
+     return value*value;
+})
+console.log(ans);
+
