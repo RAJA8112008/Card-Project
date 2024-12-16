d4eaa349adcd131678a0ae0df59148f536589453
Raja <kraj9380286@gmail.com>
2024-12-16 23:09:18 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,8 +1,8 @@
 //map in practice
 let arr = [10, 3, 5, 6, 9, 75, 20, 30, 50];
-arr.forEach((value,index)=>{
-    console.log("value:",value,"index:",index);
-});
+for(let key in arr){
+    console.log("Raja")
+};
 
 
 
