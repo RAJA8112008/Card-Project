9719bf59a08c6f5c5cdc2c831ffc96ec883315ba
Raja <kraj9380286@gmail.com>
2024-12-16 23:06:42 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,6 +1,8 @@
 //map in practice
 let arr = [10, 3, 5, 6, 9, 75, 20, 30, 50];
-arr.sort();
-console.log(arr);
+arr.forEach((value,index)=>{
+    console.log("value:",value,"index:",index);
+});
+
 
 
