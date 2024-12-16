53ef87a06818838f86b74ccf33e4ca57e8da47f6
Raja <kraj9380286@gmail.com>
2024-12-16 11:14:27 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,11 +1,11 @@
 //function practice
-let printcount = () => {
-    for (let i = 1; i <= 10; i++) { // Declare i with let
-        if (i == 8) {
-            break; // Exit the loop when i equals 8
-        } else {
-            console.log(i); // Print i otherwise
-        }
+let printcount=()=>{
+  for(let i=1;i<=10;i++){
+    if(i==6){
+        continue;
+    }else{
+        console.log(i);
     }
+  }
 };
 printcount();
