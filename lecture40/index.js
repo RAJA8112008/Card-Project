139ac9a374ac80021da4109c77254630b9a86e55
Raja <kraj9380286@gmail.com>
2024-12-16 22:30:09 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,12 +1,6 @@
-//map practice
-let obj={
-    "name":"Raja",
-    "age": 54,
-    "weight":45,
-    greet:function(){
-console.log("Happy");
-    }
-};
-for(let key in obj){
-    console.log(key);
-}
+//map in practice
+let arr=[10,20,30,50]
+arr.map((value)=>{
+    return value*value;
+   console.log(value+1)
+});
