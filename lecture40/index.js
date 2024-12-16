9505dfe956e80d4d77ff1c50cc5f2d7597d99808
Raja <kraj9380286@gmail.com>
2024-12-16 12:02:34 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,4 +1,7 @@
-//Array function practice
-let brr=[10,20,"raja","kumar"]
-brr.shift();
-console.log(brr);
+//map practice
+let arr=[10,20,30]
+let square=arr.map((number)=>{
+    return number*number;
+   
+});
+console.log(square);
