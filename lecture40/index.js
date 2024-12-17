60d58c50463d838cfa3c714078bcc805d93c58ad
Raja <kraj9380286@gmail.com>
2024-12-17 18:14:50 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,15 +1,4 @@
-//map in practice
-let arr=[10,20,30,40,50,6]
-function addnum(arr){
-    let sum=0;
-    arr.forEach((value)=>{
-         sum=sum+value;
-    })
-    return sum;
-}
-let totalsum= addnum(arr)
-console.log(totalsum);
-
-
-
-
+    function greet(){
+        console.log("Hello ji")
+    }
+    greet();
