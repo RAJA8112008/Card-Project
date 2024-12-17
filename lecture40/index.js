3b6f26692ca1fc2b2011090239de1ca7344694e4
Raja <kraj9380286@gmail.com>
2024-12-17 19:04:07 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,1 +1,11 @@
+function fullname(callback, fullname) {
+    console.log("Raaj", fullname); // Fixed the typo from "fullnmae" to "fullname"
+    callback(); // Execute the callback function
+}
 
+function name() {
+    console.log("Hello from the name function!"); // Added a meaningful message
+}
+
+// Pass the function `name` and a string `"kumar"` to `fullname`
+fullname(name, "kumar");
