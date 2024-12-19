15d3a278c17cefb9d6a379b8694171005a927216
Raja <kraj9380286@gmail.com>
2024-12-19 21:25:42 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,10 +1,3 @@
-function greet(greetme,fullname){
-    console.log("Raja",fullname)
-    console.log(greetme);
-}
-function greetme(){
-    // console.log("i am learning JS")
-}
-greet(greetme,"kumar");
+
 
 
