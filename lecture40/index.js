b03dd49b756e76305a5b538ee6b098aa17971bb7
Raja <kraj9380286@gmail.com>
2024-12-27 22:42:03 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,9 +1,1 @@
-let king=document.querySelectorAll('p');
-function alertevent(event){
-   alert("you are here");
-    
-}
-for(let i=0;i<king.length;i++){
-    console.log("Event listener Added");
-    king[i].addEventListener('click',alertevent);
-}
+
