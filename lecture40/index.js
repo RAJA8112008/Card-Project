0c2281ab1db57ebba7427527e0d4aab574a092be
Raja <kraj9380286@gmail.com>
2024-12-27 20:28:57 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,1 +1,9 @@
- 
+ let paras=document.querySelectorAll('p');
+ function alertpara(event){
+    alert("you are here");
+ }
+ for(let i=0;i<paraslength;i++){
+    let paras=paras[i];
+    console.log("eventlisteneradded");
+    paras.addeventlisner('click here');
+ }
