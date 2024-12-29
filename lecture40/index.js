dd4be171ac992ac2c116112ae0ff14083dde60a6
Raja <kraj9380286@gmail.com>
2024-12-29 18:55:19 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,11 +1,13 @@
 
 let input=document.getElementById('inputbox');
-let button=document.querySelectorAll('button');
-let string=" ";
-let arr=Array.from(button =>{
+let buttons=document.querySelectorAll('button');
+
+let string="";  
+let arr=Array.from(buttons);
+arr.forEach(button=>{
     button.addEventListner('click', (e)=>{
         if(e.target.innerHTML== '='){
-            string=eval(string);
+            string = eval(string);
             input.value=string;
         }
         else if(e.target.innerHTML == 'AC'){
@@ -16,8 +18,6 @@ let arr=Array.from(button =>{
             string=string.substring(0, string.length-1);
             input.value = string;
         }
-           
-        
         else{
             string += e.target.innerHTML;
             input.value = string;
