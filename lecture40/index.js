733ea6ee67f3ca6b0a8b66259d9b78d457df29c0
Raja <kraj9380286@gmail.com>
2024-12-29 18:16:51 +0530

    commited

--- lecture40/index.js
+++ lecture40/index.js
@@ -1,9 +1,14 @@
 
-let mypara = document.querySelectorAll('p');
-function alertpara(event){
-alert("you clicked here ;"+ event.target.textContent);
-}
-for(let i=0; i< mypara.length;i++){
-    console.log("Event Listener added");
-   mypara[i].addEventListener('click',alertpara);
-}
+let input=document.getElementById('inputbox');
+let button=document.querySelectorAll('button');
+let string=" ";
+let arr=Array.from(button =>{
+    button.addEventListner('click', (e)=>{
+        if(e.target.innerHTML== '='){
+            string=eval(string);
+            input.value=string;
+        }
+        string += e.target.innerHTML;
+        input.value = string;
+    })
+} )
