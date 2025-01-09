9a4c2b022bf91aceba7efc56aa67ca39c50d80f6
Raja <kraj9380286@gmail.com>
2025-01-09 13:46:15 +0530

    commited

--- project 8/index.js
+++ project 8/index.js
@@ -1,5 +1,5 @@
 let key ="f7137cad736f228e66fc0dc27f82585a"
-let input =document.getElementById("#input")
+let Cityinput =document.getElementById("Cityinput")
 let btn =document.getElementById("search-btn")
 let city =document.getElementById("city-name")
 let weatherInfoCard= document.getElementById("weather-info")
@@ -7,3 +7,11 @@ let temperature =document.getElementById("temperature")
 let descripton=document.getElementById("descripton")
 let erroeMessage= document.getElementById("erroe-message")
 let icon = document.getElementById("weather-icon")
+
+btn.addEventListener("click",()=>{
+    const city= Cityinput.value
+   
+})
+async function getweather(city){
+const responce =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=$(city)`)
+}
