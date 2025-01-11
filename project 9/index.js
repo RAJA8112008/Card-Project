0faa79d8776e271d25e745cb1d727c7d98c77c22
Raja <kraj9380286@gmail.com>
2025-01-11 14:17:18 +0530

    commited

---
+++ project 9/index.js
@@ -0,0 +1,51 @@
+
+
+const quizData=[
+    {
+        question:"what does HTML stands for?",
+        option :[
+            "Hypertext Markup Language",
+            "Hyper Transfer Markup Language",
+            "Hypertext Machine Language",
+            "Hyperlink and Text Markup Language",
+        ],
+        correct: 0,
+    },
+    {
+        question:"what does HTML stands for?",
+        option :[
+            "Hypertext Markup Language",
+            "Hyper Transfer Markup Language",
+            "Hypertext Machine Language",
+            "Hyperlink and Text Markup Language",
+        ],
+        correct: 0,
+    },
+    {
+        question:"what does HTML stands for?",
+        option :[
+            "Hypertext Markup Language",
+            "Hyper Transfer Markup Language",
+            "Hypertext Machine Language",
+            "Hyperlink and Text Markup Language",
+        ],
+        correct: 0,
+    },
+    {
+        question:"what does HTML stands for?",
+        option :[
+            "Hypertext Markup Language",
+            "Hyper Transfer Markup Language",
+            "Hypertext Machine Language",
+            "Hyperlink and Text Markup Language",
+        ],
+        correct: 0,
+    },
+];
+
+const  answerElm =document.querySelectorAll('.answer');
+const [questionElm, option_1, option_2, option_3,option_4]= document.querySelectorAll("#question, .option_1, .option_2, .option_3, .option_4");
+const submitBtn = document.querySelector("#submit");
+
+const currentQuiz =0;
+const score =0;
